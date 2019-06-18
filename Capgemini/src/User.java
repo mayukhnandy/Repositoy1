bc4d701584b5180e65d79e176f421044c23f0977
mayukhnandy <mayukhnandy13@gmail.comv>
2019-06-18 15:55:18 +0530

    a

---
+++ Capgemini/src/User.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author
+ *
+ */
+public class User {
+
+	/**
+	 * 
+	 */
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
