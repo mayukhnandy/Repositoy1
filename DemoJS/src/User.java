14db87798c7c92c36a3694773a2318e88c0d59e7
vg262 <vg262@demo1.sr.kovair.com>
2019-08-23 03:01:58 -0700

    19

--- DemoJS/src/User.java
+++ DemoJS/src/User.java
@@ -1,25 +1,121 @@
-/**
- * 
- */
+public class User
+{
+    private double startingMoney;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
 
-/**
- * @author
- *
- */
-public class User {
+    private static int count = 0; // number of objects in memory
 
-	/**
-	 * 
-	 */
-	public User() {
-		// TODO Auto-generated constructor
-	}
+    // initialize user, add 1 to static count and
+     // output String indicating that constructor was called
+     public User( String first, String last )
+     {
+        firstName = first;
+        lastName = last;
 
-	/**
-	 * @param argfsiugy
-	 */
-	public static void main(String[] args) {
-		// de
-	}
+        count++; // increment static count of Users
+        System.out.printf( "User constructor: %s %s; count = %d\n",
+           firstName, lastName, count );
+     } // end User constructor
 
-}
+
+     private double startingMoney;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+
+     //another constructer 
+     public User( Double sMoney, String first, String last, String name, String pass)
+     {
+         startingMoney = sMoney
+         firstName = first;
+         lastName = last;
+         username = name;
+         password = pass;
+
+         count++; // increment static count of Users
+         System.out.printf( "User constructor: %s %s; count = %d\n",
+            firstName, lastName, count );
+      } // end User constructor
+
+
+      // subtract 1 from static count when garbage
+      // collector calls finalize to clean up object;
+      // confirm that finalize was called
+      protected void finalize()
+      {
+         count--; // decrement static count of users
+         System.out.printf( "User finalizer: %s %s; count = %d\n",
+            firstName, lastName, count );
+      } // end method finalize
+
+    // set first name
+      public String setFirstName(String firstName)
+      {
+         this.firstName = firstName;
+      } // end method setFirstName
+
+      // set last name
+      public String setLastName(String lastName)
+      {
+         this.lastName = lastName;
+      } // end method setLastName
+
+     // set user name
+      public String setUserName(String name)
+      {
+         this.username = name;
+      } // end method setUserName
+
+      // set password
+      public String setPassword(String password)
+      {
+         this.password = password;
+      } // end method setPassword
+
+
+
+
+
+
+      public String getStartingName()
+      {
+         return startingName;
+      } // end method getstartingName
+
+
+     // get username
+      public String getUserName()
+      {
+         return username;
+      } // end method getUserName
+
+      // get password
+      public String getPassword()
+      {
+         return password;
+      } // end method getPassword
+
+     // get first name
+      public String getFirstName()
+      {
+         return firstName;
+      } // end method getFirstName
+
+      // get last name
+      public String getLastName()
+      {
+         return lastName;
+      } // end method getLastName
+
+      // static method to get static count value
+      public static int getCount()              
+      {                                         
+         return count;                          
+      } // end method getCount
+
+ }
