f901decf6c2e35837522273b1f14f1ab7bd706b1
vg262 <vg262@demo1.sr.kovair.com>
2019-08-26 05:43:24 -0700

    24

--- DemoJS/src/User.java
+++ DemoJS/src/User.java
@@ -116,6 +116,6 @@ public class User
       public static int getCount()              
       {                                         
          return count;                          
-      } // end method getCount gbgtrbrtjhyfjuy
+      } // end method getCount gbgtrbrtjhyfjuyjhgfjh
 
  }
