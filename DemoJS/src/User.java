87a0c96f0b44f6e3453f48b35b96fb29dce58d70
ac289 <mayukhnandy13@gmail.com>
2019-10-16 06:02:13 -0700

    hy

--- DemoJS/src/User.java
+++ DemoJS/src/User.java
@@ -9,7 +9,7 @@ public class User
     private static int count = 0; // number of objects in memory SGD
 
  
-     // output String indicating that constructor was called
+     // output String indicating that constructor was called.
      public User( String first, String last )
      {
         firstName = first;
