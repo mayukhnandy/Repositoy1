0233957803cfc3a1625b736d1c4227e992251d60
sm281 <sm281@demo1.sr.kovair.com>
2019-11-18 03:53:18 -0800

    46

--- Automation.java
+++ Automation.java
@@ -25,7 +25,7 @@ public class Automation <wishlist> {
 	}
 	
 	public void removeFromWishlist(Item item){
-		wishlist.remove(item);					//wishlist Item Removed(modified)BR 48
+		wishlist.remove(item);					//wishlist Item Removed(modified)BR 46
 	}
       
 	public User getTasteManager(){
