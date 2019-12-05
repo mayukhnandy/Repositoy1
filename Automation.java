a955c29c821082d0337047c7f13ffc754f3ec592
sujoyghosh26 <sghosh@kovair.com>
2019-12-05 04:44:01 -0800

    KDP-1230

--- Automation.java
+++ Automation.java
@@ -25,7 +25,7 @@ public class Automation <wishlist> {
 	}
 	
 	public void removeFromWishlist(Item item){
-		wishlist.remove(item);					// US41 again SGD mod
+		wishlist.remove(item);					// US41 again SGD modification
 	}
       
 	public User getTasteManager(){
