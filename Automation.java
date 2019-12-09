aa9c7aad82a2cbec7c617d4b6315c7f8dd6a82db
sm281 <sm281@demo1.sr.kovair.com>
2019-12-09 01:23:12 -0800

    BDP-151

--- Automation.java
+++ Automation.java
@@ -25,7 +25,7 @@ public class Automation <wishlist> {
 	}
 	
 	public void removeFromWishlist(Item item){
-		wishlist.remove(item);					// BDP-151 mod
+		wishlist.remove(item);					// BDP-151 modified
 	}
       
 	public User getTasteManager(){
