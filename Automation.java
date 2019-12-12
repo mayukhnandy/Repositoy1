8efc028394537bd153be7d3249c62f52dcfc35f8
sm281 <sm281@demo1.sr.kovair.com>
2019-12-12 01:33:00 -0800

    1008

--- Automation.java
+++ Automation.java
@@ -25,7 +25,7 @@ public class Automation <wishlist> {
 	}
 	
 	public void removeFromWishlist(Item item){
-		wishlist.remove(item);					//US1006
+		wishlist.remove(item);					//US1008
 	}
       
 	public User getTasteManager(){
